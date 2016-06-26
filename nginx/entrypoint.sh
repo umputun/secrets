e4be66700957573cb5cfd4f624d18ba1445011e9
umputun <umputun@gmail.com>
2016-06-26 16:20:42 -0500

    automatic key deployment in runtime

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 echo "start nginx"
+
+echo "ssl_key=${SSL_KEY:=/etc/nginx/ssl/secrets-key.pem}, ssl_cert=${SSL_CERT:=/etc/nginx/ssl/secrets-crt.pem}"
+
+sed -i "s/SECRETS_KEY/${SSL_KEY}/g" /etc/nginx/conf.d/secrets.conf
+sed -i "s/SECRETS_CERT/${SSL_CERT}/g" /etc/nginx/conf.d/secrets.conf
+
 nginx -g "daemon off;"
