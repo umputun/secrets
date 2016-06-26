6849797a7e50c31b91bdd17182f2a28eb8dad008
umputun <umputun@gmail.com>
2016-06-26 17:07:34 -0500

    force robots copy

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -6,4 +6,6 @@ echo "ssl_key=${SSL_KEY:=/etc/nginx/ssl/secrets-key.pem}, ssl_cert=${SSL_CERT:=/
 sed -i "s|SECRETS_KEY|${SSL_KEY}|g" /etc/nginx/conf.d/secrets.conf
 sed -i "s|SECRETS_CERT|${SSL_CERT}|g" /etc/nginx/conf.d/secrets.conf
 
+cp -f /robots.txt /srv/docroot/robots.txt
+
 nginx -g "daemon off;"
