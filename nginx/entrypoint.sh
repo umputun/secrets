1a78beb8dd29d9b18276fe7c74247489509e3b1b
Eugene <eugene@umputun.com>
2016-06-26 23:50:32 -0500

    letsencrypt should be wokring

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -9,4 +9,16 @@ sed -i "s|SECRETS_KEY|${SSL_KEY}|g" /etc/nginx/conf.d/secrets.conf
 sed -i "s|SECRETS_CERT|${SSL_CERT}|g" /etc/nginx/conf.d/secrets.conf
 
 cp -f /robots.txt /srv/docroot/robots.txt
+
+(
+ sleep 3 #give nginx time to start
+ echo "start letsencrypt updater"
+ while :
+ do
+	echo "trying to update letsencrypt ..."
+    /le.sh
+    sleep 60d
+ done
+) &
+
 nginx -g "daemon off;"
---
+++ nginx/le.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "$LETSENCRYPT" = "true" ];
+    letsencrypt certonly -t -n --agree-tos --email "${LE_EMAIL}" --webroot -w /usr/share/nginx/html -d $LE_FQDN -v
+    cp -fv /etc/letsencrypt/live/$LE_FQDN/privkey.pem /etc/nginx/ssl/secrets-key.pem
+    cp -fv /etc/letsencrypt/live/$LE_FQDN/fullchain.pem /etc/nginx/ssl/secrets-cert.pem
+else
+    echo "letsencrypt disabled"
+fi
