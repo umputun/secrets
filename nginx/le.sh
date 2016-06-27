c932e7dd5c725dbda4358ce8f020503ea441548e
Eugene <eugene@umputun.com>
2016-06-26 23:54:59 -0500

    missing then

--- nginx/le.sh
+++ nginx/le.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$LETSENCRYPT" = "true" ];
+if [ "$LETSENCRYPT" = "true" ]; then
     letsencrypt certonly -t -n --agree-tos --email "${LE_EMAIL}" --webroot -w /usr/share/nginx/html -d $LE_FQDN -v
     cp -fv /etc/letsencrypt/live/$LE_FQDN/privkey.pem /etc/nginx/ssl/secrets-key.pem
     cp -fv /etc/letsencrypt/live/$LE_FQDN/fullchain.pem /etc/nginx/ssl/secrets-cert.pem
