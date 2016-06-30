5b2f9845b967b231c2ae1b738b3279b29a986657
Eugene <eugene@umputun.com>
2016-06-30 00:56:00 -0500

    .

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -8,6 +8,11 @@ SSL_CERT=/etc/nginx/ssl/${SSL_CERT}
 sed -i "s|SECRETS_KEY|${SSL_KEY}|g" /etc/nginx/conf.d/secrets.conf
 sed -i "s|SECRETS_CERT|${SSL_CERT}|g" /etc/nginx/conf.d/secrets.conf
 
+cd /etc/nginx/ssl
+echo "make dhparams"
+openssl dhparam -out dhparams.pem 2048
+chmod 600 dhparams.pem
+
 cp -f /robots.txt /srv/docroot/robots.txt
 
 #disable ssl configuration and let it run without SSL
